2a32ceca8ab97f4b7d29567053c9722c01164635
JiaYue-Ong <jia.ong.21@ucl.ac.uk>
2025-04-02 12:48:42 -0400

    draft 1
    Uploading the files of all my work. Will provide detailed explanation later

---
+++ tasks/phl_property_data_pipeline/extract_phl_opa_properties.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import pathlib
+import requests
+from google.cloud import storage
+
+
+DATA_DIR = pathlib.Path(__file__).parent / 'raw_data'
+
+# Download the OPA Properties data as a CSV
+url = 'https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv'
+filename = DATA_DIR / 'phl_opa_properties.csv'
+
+response = requests.get(url)
+response.raise_for_status()
+
+with open(filename, 'wb') as f:
+    f.write(response.content)
+
+print(f'Downloaded {filename}')
+
+# Upload the download file to cloud storage
+bucket_name = os.getenv('RAW_DATA_BUCKET')
+blobname = 'phl_opa_properties/phl_opa_properties.csv'
+
+storage_client = storage.Client()
+bucket = storage_client.bucket(bucket_name)
+blob = bucket.blob(blobname)
+blob.upload_from_filename(filename)
+
+print(f'Uploaded {filename} to gs://{bucket_name}/{blobname}')
